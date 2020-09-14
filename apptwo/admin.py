e14fce61d5a28a873fa80a5e846c305d7e54a46f
MUNSI Patrick <patrick.munsi@ethias.be>
2020-09-14 21:12:34 +0200

    admin.site.register(Employee) admin.site.register(Company)

--- apptwo/admin.py
+++ apptwo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from appone.models import Employee
+from appone.models import Company
+
+admin.site.register(Employee)
+admin.site.register(Company)
+
+#python manage.py shell
+#python manage.py createsuperuser
